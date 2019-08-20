bebabcbeeeeac05cb3180f3db3e76cea22ab2add
yong.wei <weiyong@youxin.com>
2019-08-20 17:16:13 +0800

    添加获取IP和端口接口

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const ip = require('ip');
 
 app.get('/', (req, res) => {
   res.send('ok');
@@ -8,6 +9,11 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
   res.send('ok');
+});
+
+app.get('/api/sys-info', (req, res) => {
+  let result = {ip:ip.address(), port:PORT};
+  res.send(JSON.stringify(result));
 });
 
 app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
