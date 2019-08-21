1c402f484b6e80eac1ed7a69d0fda672a1a5c474
yong.wei <weiyong@youxin.com>
2019-08-21 10:30:38 +0800

    支持代理

--- server.js
+++ server.js
@@ -2,18 +2,42 @@ const app = require('express')();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const ip = require('ip');
-
-app.get('/', (req, res) => {
-  res.send('ok');
-});
+const request = require('request');
+const http = require('http');
 
 app.get('/health', (req, res) => {
   res.send('ok');
 });
 
 app.get('/api/sys-info', (req, res) => {
-  let result = {ip:ip.address(), port:PORT};
+  let result = {ip: ip.address(), port: PORT};
   res.send(JSON.stringify(result));
+});
+
+app.use(function (req, res) {
+  if (req.method.toLocaleLowerCase() === 'get') {
+    request(req.url, (err, response, body) => {
+      if (err) {
+        res.send(err);
+      }
+      if (body) {
+        res.set(response.headers);
+        res.send(body);
+      }
+    })
+  } else if (req.method.toLocaleLowerCase() === 'post') {
+    request.post(req.url, (err, response, body) => {
+      if (err) {
+        res.send(err);
+      }
+      if (body) {
+        res.set(response.headers);
+        res.send(body);
+      }
+    })
+  }else {
+    res.send(JSON.stringify({method:req.method,error:'request not support now'}));
+  }
 });
 
 app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
