ba13a40b7a2e07c57d0e5b83e363f0ce6dbd53c9
yong.wei <weiyong@youxin.com>
2019-09-24 19:36:45 +0800

    完成增删改查操作

--- routes/countDay.js
+++ routes/countDay.js
@@ -26,28 +26,58 @@ const CountDay = require('../models/CountDay.js');
 //  res.send(result);
 //});
 
-router.get('/records', async (req, res) => {
-  const result = await CountDay.findAll();
+/**
+ * 查询倒数日记录
+ */
+router.post('/list', async (req, res) => {
+  const params = req.body;
+  if (!params.uid) {
+    res.send(Errors.paramError(`uid error: ${params.uid}`));
+  }
+  const result = await CountDay.findAll({where: {uid: params.uid}});
   res.send(result);
 });
 
+/**
+ * 增加倒数日记录
+ */
 router.post('/add', async (req, res) => {
    const params = req.body;
    const errors = Interface.CountDayRecord.validate(params);
    if (errors.length) {
      res.send(Errors.paramError(errors[0].message));
-   }else {
-     let date = new Date();
-     const result = await CountDay.create({
-       id:Date.now(),
-       event_name: params.eventName,
-       event_time: params.eventTime,
-       uid: params.uid,
-       created: date,
-       updated: date
-     });
-     res.send(Errors.serverOK());
    }
+   let date = new Date();
+   const result = await CountDay.create({
+     id:Date.now(),
+     event_name: params.eventName,
+     event_time: params.eventTime,
+     uid: params.uid,
+     created: date,
+     updated: date
+   });
+   res.send(Errors.serverOK());
 });
+
+/**
+ * 删除倒数日记录
+ */
+router.post('/remove', async (req, res) => {
+  const params = req.body;
+  if (!params.id) {
+    res.send(Errors.paramError(`record id error: ${params.id}`));
+  }
+  const result = await CountDay.destroy({where: {id: params.id}});
+  res.send(Errors.serverOK());
+});
+
+router.post('/edit', async (req, res) =>{
+  const params = req.body;
+  const errors = Interface.CountDayRecord.validate(params);
+  if (errors.length) {
+    res.send(Errors.paramError(errors[0].message));
+  }
+});
+
 
 module.exports = router;
